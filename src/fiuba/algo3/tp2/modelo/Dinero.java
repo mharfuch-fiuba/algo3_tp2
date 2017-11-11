110ab245aa9826e9ae81848eae2ee99f612293db
matias <matias@localhost.localdomain>
2017-11-11 15:13:37 -0300

    implementacion de dinero

--- src/fiuba/algo3/tp2/modelo/Dinero.java
+++ src/fiuba/algo3/tp2/modelo/Dinero.java
@@ -1,5 +1,16 @@
 package fiuba.algo3.tp2.modelo;
 
-public class Dinero {
+public class Dinero implements Comparable<Dinero> {
+
+	private Double cantidad = (double) 0;
+	
+	@Override
+	public int compareTo(Dinero otro) {
+		return this.cantidad.compareTo(otro.getCantidad());
+	}
+
+	private Double getCantidad() {
+		return this.cantidad;
+	}
 	
 }
