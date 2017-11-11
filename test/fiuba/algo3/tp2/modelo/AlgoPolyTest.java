7a2bf6005fdca8fba23315d8e68cf33d4555b6ad
matias <matias@localhost.localdomain>
2017-11-11 11:18:37 -0300

    Complete de vuelta la clase

--- test/fiuba/algo3/tp2/modelo/AlgoPolyTest.java
+++ test/fiuba/algo3/tp2/modelo/AlgoPolyTest.java
@@ -1,5 +1,24 @@
 package fiuba.algo3.tp2.modelo;
 
-public class AlgoPolyTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class AlgoPolyTest {
+	
+	@Test
+	public void Test01elCapitalSeIncrementaEnQuini6() {
+		Recorrible tablero = new TableroAlgoPoly();
+		iterTablero iterador = new IterTablero(tablero);
+		Jugador jugador = new JugadorHumano(iterador);
+		Dado dado1 = new DadoCargado(7);
+		Dado dado2 = new DadoCargado(14);
+		Cubilete cubilete = new Cubilete();
+		cubilete.agregar(dado1);
+		cubilete.agregar(dado2);
+		cubilete.lanzar();
+		jugador.avanzar(cubilete);
+		Dinero monto_final = jugador.obtenerDinero();
+		Assert.assertEquals(150000, monto_final);
+	}
+	
 }
