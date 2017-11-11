79a02ba548474b5c5faa06a50d6b4a6377b0ba9e
matias <matias@localhost.localdomain>
2017-11-11 20:28:36 -0300

    Marco para pruebas de integracion

--- test/fiuba/algo3/tp2/modelo/AlgoPolyTest.java
+++ test/fiuba/algo3/tp2/modelo/AlgoPolyTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class AlgoPolyTest {
 	
 	@Test
-	public void Test01elCapitalSeIncrementaEnQuini6() {
+	public void Test01_ElCapitalSeIncrementaEn50000AlCaerEnQuini6PorPrimeraVez() {
 		Tablero tablero = new TableroAlgoPoly();
 		Jugador jugador = new JugadorHumano(tablero);
 		Lanzable dado1 = new DadoCargado(7);
@@ -19,6 +19,61 @@ public class AlgoPolyTest {
 		double monto_final = jugador.obtenerDinero().getCantidad();
 		double monto_esperado = new Dinero(150000).getCantidad();
 		Assert.assertEquals(monto_esperado, monto_final, 0.001);//NO SE BIEN COMO SE HACE ESTO CON EQUALS
+	}
+	
+	@Test
+	public void Test02_ElCapitalSeIncrementaEn30000AlCaerEnQuini6PorSegundaVez() {
+		
+	}
+	
+	@Test
+	public void Test03_ElCapitalNoSeIncrementaAlCaerEnQuini6PorTerceraVez() {
+		
+	}
+	
+	@Test
+	public void Test04_ElJugadorCompraUnBarrio() {
+		
+	}
+	
+	@Test
+	public void Test05_ElJugadorQueCaeEnLaCarcelNoPuedeMoverse() {
+		
+	}
+	
+	@Test
+	public void Test06_ElJugadorQuePagaLaFianzaPuedeMoverse() {
+		
+	}
+	
+	@Test
+	public void Test07_ElJugadorQueNoPuedePagarLaFianzaPorFaltaDeFondosNoPuedeMoverse() {
+		
+	}
+	
+	@Test
+	public void Test08_ElJugadorQueCaeEnAvanceDinamicoHabiendoSacado6Avanza4Casilleros() {
+		
+	}
+	
+	@Test
+	public void Test09_ElJugadorQueCaeEnAvanceDinamicoHabiendoSacado7Avanza5CasillerosSiSuCapitalEsDe100000() {
+		
+	}
+	
+	@Test
+	public void Test10_ElJugadorQueCaeEnAvanceDinamicoHabiendoSacado12Avanza10CasillerosSiTiene2Propiedades() {
+		
+	}
+	
+	@Test
+	public void Test11() {
+		
+	}
+	
+	@Test
+	public void Test12_ElJugadorQueCaeEnPoliciaVaALaCarcel() {
+		
 	}
 	
 }
