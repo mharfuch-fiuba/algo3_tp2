9b64d0572646516ad0679b4f3ac642ab42f0631f
MRicaldone <MRicaldone@DESKTOP-6HVH4EJ>
2017-11-14 20:22:48 -0300

    Tests unitarios para tableroalgopoly

--- test/fiuba/algo3/tp2/modelo/TableroAlgoPolyTest.java
+++ test/fiuba/algo3/tp2/modelo/TableroAlgoPolyTest.java
@@ -1,23 +1,57 @@
 package fiuba.algo3.tp2.modelo;
 
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fiuba.algo3.tp2.modelo.encasillables.*;
+import fiuba.algo3.tp2.modelo.encasillables.comprables.*;
+
 public class TableroAlgoPolyTest {
-/* 	PROMETO HACER DE VUELTA LOS TESTS
- * 
-	Tablero tablero = new TableroAlgoPoly();
-	Encasillable salida = tablero.getSalida();
-	
+
 	@Test
-	public void getCasilleroDestinoConOrigenSalidaY3SaltosDevuelveEdesur(){
-		Encasillable destinoEdesur = tablero.getCasilleroDestino(salida, 3);
-		Assert.assertTrue(destinoEdesur instanceof Edesur);
+	public void testCrearIteradorNoEsNulo() {
+		Tablero tablero = new TableroAlgoPoly();
+		IterTablero iterador = tablero.crearIterador();
+		Assert.assertNotNull(iterador);
 	}
 	
 	@Test
-	public void getCasilleroDestinoConOrigenTucumanY2SaltosDevuelveQuini6(){
-		//Esto verifica que sea circular
-		Encasillable origenTucuman =tablero.getCasilleroDestino(salida, 19);
-		Encasillable destinoQuini6 = tablero.getCasilleroDestino(origenTucuman, 2);
-		Assert.assertTrue(destinoQuini6 instanceof Quini6);
+	public void testCrearIteradorTieneLaSecuenciaCorrectaDeCasilleros() {
+		Tablero tablero = new TableroAlgoPoly();
+		IterTablero iterador = tablero.crearIterador();
+		ArrayList<Encasillable> casilleros = new ArrayList<Encasillable>();
+		for(int i = 0; i < 20; i++) {
+			casilleros.add(iterador.verActual());
+			iterador.avanzar();
+		}
+		Assert.assertTrue(casilleros.get(0) instanceof Salida);
+		Assert.assertTrue(casilleros.get(1) instanceof Quini6);
+		Assert.assertTrue(casilleros.get(2) instanceof BuenosAiresSur);
+		Assert.assertTrue(casilleros.get(3) instanceof Edesur);
+		Assert.assertTrue(casilleros.get(4) instanceof BuenosAiresNorte);
+		Assert.assertTrue(casilleros.get(5) instanceof Carcel);
+		Assert.assertTrue(casilleros.get(6) instanceof CordobaSur);
+		Assert.assertTrue(casilleros.get(7) instanceof AvanceDinamico);
+		Assert.assertTrue(casilleros.get(8) instanceof Subte);
+		Assert.assertTrue(casilleros.get(9) instanceof CordobaNorte);
+		Assert.assertTrue(casilleros.get(10) instanceof ImpuestoDeLujo);
+		Assert.assertTrue(casilleros.get(11) instanceof SantaFe);
+		Assert.assertTrue(casilleros.get(12) instanceof Aysa);
+		Assert.assertTrue(casilleros.get(13) instanceof SaltaNorte);
+		Assert.assertTrue(casilleros.get(14) instanceof SaltaSur);
+		Assert.assertTrue(casilleros.get(15) instanceof Policia);
+		Assert.assertTrue(casilleros.get(16) instanceof Tren);
+		Assert.assertTrue(casilleros.get(17) instanceof Neuquen);
+		Assert.assertTrue(casilleros.get(18) instanceof RetrocesoDinamico);
+		Assert.assertTrue(casilleros.get(19) instanceof Tucuman);
 	}
-*/	
+
+	@Test
+	public void testTableroAlgoPolyNoEsNuloAlCrear() {
+		Tablero tablero = new TableroAlgoPoly();
+		Assert.assertNotNull(tablero);
+	}
+
 }
