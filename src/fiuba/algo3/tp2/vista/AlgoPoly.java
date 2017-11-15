ad37752640ae466822ff5e0712d684cbc3ead1c6
FedeZugna <fede.zugna@gmail.com>
2017-11-15 15:55:13 -0300

    Agrego posible pantalla de Bienvenidos con el Logo

--- src/fiuba/algo3/tp2/vista/AlgoPoly.java
+++ src/fiuba/algo3/tp2/vista/AlgoPoly.java
@@ -10,28 +10,38 @@ public class AlgoPoly extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		
-		ControladorPartida controladorPartida = new ControladorPartida();
-
-		ContenedorPartida vistaPartida = new ContenedorPartida(stage,controladorPartida);	
-		Scene escenaPartida = new Scene(vistaPartida);
-		
-		VistaConfiguracionPartida vistaConfiguracionPartida= new VistaConfiguracionPartida(stage,controladorPartida,escenaPartida);
-		Scene escenaConfiguracionPartida = new Scene(vistaConfiguracionPartida);
-		
-		VistaInicial vistaInicial = new VistaInicial(stage,escenaConfiguracionPartida);
-		Scene escenaInicial = new Scene(vistaInicial);
-		
-		escenaInicial.getStylesheets().add("fiuba/algo3/tp2/vista/styles.css");
-		escenaConfiguracionPartida.getStylesheets().add("fiuba/algo3/tp2/vista/styles.css");
-		escenaPartida.getStylesheets().add("fiuba/algo3/tp2/vista/styles.css");
-		//escenaPartida.setOnKeyPressed(new AplicacionOnKeyPressEventHandler(stage));
-		
-		stage.setTitle("AlgoPoly");
-		stage.setScene(escenaInicial);
-		stage.show();
-		stage.setResizable(false);
-		
+		try{
+			
+			ControladorPartida controladorPartida = new ControladorPartida();
+	
+			ContenedorPartida vistaPartida = new ContenedorPartida(stage,controladorPartida);	
+			Scene escenaPartida = new Scene(vistaPartida);
+			
+			VistaConfiguracionPartida vistaConfiguracionPartida= new VistaConfiguracionPartida(stage,controladorPartida,escenaPartida);
+			Scene escenaConfiguracionPartida = new Scene(vistaConfiguracionPartida);
+			
+			/*VistaInicial vistaInicial = new VistaInicial(stage,escenaConfiguracionPartida);
+			Scene escenaInicial = new Scene(vistaInicial);
+			
+			escenaInicial.getStylesheets().add("fiuba/algo3/tp2/vista/styles.css");
+			escenaConfiguracionPartida.getStylesheets().add("fiuba/algo3/tp2/vista/styles.css");
+			escenaPartida.getStylesheets().add("fiuba/algo3/tp2/vista/styles.css");
+			//escenaPartida.setOnKeyPressed(new AplicacionOnKeyPressEventHandler(stage));
+			
+			stage.setTitle("AlgoPoly");
+			stage.setScene(escenaInicial);
+			stage.show();
+			stage.setResizable(false);*/
+			
+			ContenedorBienvenidos contenedorBienvenidos = new ContenedorBienvenidos(stage, escenaPartida);
+			Scene escenaBienvenidos = new Scene(contenedorBienvenidos, 800, 600);
+			
+			stage.setScene(escenaBienvenidos);
+			stage.setFullScreen(true);
+			stage.show();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
