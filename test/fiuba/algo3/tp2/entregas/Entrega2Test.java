9467f0ec3722c625a9a6f634488010dc7c2c64b3
MRicaldone <MRicaldone@DESKTOP-6HVH4EJ>
2017-11-22 20:51:41 -0300

    Hice el test 3 de otra forma, pero ahora no pasa.

--- test/fiuba/algo3/tp2/entregas/Entrega2Test.java
+++ test/fiuba/algo3/tp2/entregas/Entrega2Test.java
@@ -9,6 +9,7 @@ import fiuba.algo3.tp2.modelo.Cubilete;
 import fiuba.algo3.tp2.modelo.DadoCargado;
 import fiuba.algo3.tp2.modelo.Dinero;
 import fiuba.algo3.tp2.modelo.DineroAlgoPoly;
+import fiuba.algo3.tp2.modelo.Encasillable;
 import fiuba.algo3.tp2.modelo.Jugador;
 import fiuba.algo3.tp2.modelo.JugadorHumano;
 import fiuba.algo3.tp2.modelo.Lanzable;
@@ -123,6 +124,41 @@ public class Entrega2Test {
 		jugador200k.aplicarEfectoDeCasilleroActual(cubilete);
 		int dineroDespues = jugador200k.obtenerDinero().obtenerMontoEntero();
 		Assert.assertEquals(197000, dineroDespues);
+		// DE OTRA FORMA ... <PRUEBA DE INTEGRACION>
+		final int monto_inicial = 200000;
+		
+		Cubilete cubilete_que_saca_2 = new Cubilete();
+		cubilete_que_saca_2.agregar(new DadoCargado(2));
+		
+		Jugador jugador1 = new JugadorHumano(tablero, new DineroAlgoPoly(monto_inicial));
+		jugador1.avanzar(cubilete_que_saca_2.sumarValores());
+		Construible bsas_sur = (Construible) jugador1.obtenerCasilleroActual();
+		jugador1.comprarCasilleroActual();//Compra BsAs Sur $20000
+		int monto_esperado_jugador_1 = monto_inicial - 20000;
+		Assert.assertEquals(monto_esperado_jugador_1, jugador1.obtenerDinero().obtenerMontoEntero());
+		//SI INTENTO CONSTRUIR NO ME DEBERIA DEJAR PORQUE ME FALTA BSAS NORTE, NO?
+		jugador1.avanzar(cubilete_que_saca_2.sumarValores());
+		jugador1.comprarCasilleroActual();//Compra BsAs Norte $25000
+		monto_esperado_jugador_1 = monto_esperado_jugador_1 - 25000;
+		Assert.assertEquals(monto_esperado_jugador_1, jugador1.obtenerDinero().obtenerMontoEntero());
+		Construible bsas_nor = (Construible) jugador1.obtenerCasilleroActual();
+		bsas_sur.construir();
+		monto_esperado_jugador_1 = monto_esperado_jugador_1 - 5000;
+		Assert.assertEquals(monto_esperado_jugador_1, jugador1.obtenerDinero().obtenerMontoEntero());
+		bsas_nor.construir();
+		monto_esperado_jugador_1 = monto_esperado_jugador_1 - 5500;
+		Assert.assertEquals(monto_esperado_jugador_1, jugador1.obtenerDinero().obtenerMontoEntero());
+		
+		Jugador jugador2 = new JugadorHumano(tablero, new DineroAlgoPoly(monto_inicial));
+		jugador2.avanzar(cubilete_que_saca_2.sumarValores());
+		jugador2.aplicarEfectoDeCasilleroActual(cubilete_que_saca_2);
+		int monto_esperado_jugador_2 = monto_inicial - 2000;
+		Assert.assertEquals(monto_esperado_jugador_2, jugador2.obtenerDinero().obtenerMontoEntero());
+		jugador2.avanzar(cubilete_que_saca_2.sumarValores());
+		jugador2.aplicarEfectoDeCasilleroActual(cubilete_que_saca_2);
+		monto_esperado_jugador_2 = monto_esperado_jugador_2 - 2500;
+		Assert.assertEquals(monto_inicial - 4500, jugador2.obtenerDinero().obtenerMontoEntero());
+		
 	}
 
 	@Test
