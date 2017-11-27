44bf332b02e24450155c6329808af5dee4d3a2e6
FedeZugna <fede.zugna@gmail.com>
2017-11-27 10:49:51 -0300

    Boton Salir

---
+++ src/fiuba/algo3/tp2/vista/botones/BotonSalir.java
@@ -0,0 +1,14 @@
+package fiuba.algo3.tp2.vista.botones;
+
+
+import javafx.scene.control.Button;
+
+
+
+public class BotonSalir extends Button{
+
+	public BotonSalir(){
+		this.setText("SALIR");
+		this.getStyleClass().add("BotonSalir");
+	}
+}
