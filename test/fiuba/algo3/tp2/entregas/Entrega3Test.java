b4d3f9b0ba4f727e290e771af66a66eaf4b72a68
Matias Ricaldone <mricaldone@gmail.com>
2017-11-30 14:16:41 -0300

    Test 3era entrega

---
+++ test/fiuba/algo3/tp2/entregas/Entrega3Test.java
@@ -0,0 +1,59 @@
+package fiuba.algo3.tp2.entregas;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fiuba.algo3.tp2.modelo.Dinero;
+import fiuba.algo3.tp2.modelo.Jugador;
+import fiuba.algo3.tp2.modelo.JugadorHumano;
+import fiuba.algo3.tp2.modelo.cubilete.Cubilete;
+import fiuba.algo3.tp2.modelo.cubilete.DadoCargado;
+import fiuba.algo3.tp2.modelo.tablero.Encasillable;
+import fiuba.algo3.tp2.modelo.tablero.Tablero;
+
+public class Entrega3Test {
+	
+	public Tablero tablero = new Tablero();
+	
+	@Test
+	public void test01_JugadorArrojaDosDadosTienenElMismoValorVuelveAJugar() {
+		
+	}
+	
+	@Test
+	public void test02_JugadorArrojaDosDadosTienenElMismoValorDeVueltaVuelveAJugar() {
+		
+	}
+	
+	@Test
+	public void test03_NOENTENDI() {
+		
+	}
+	
+	@Test
+	public void test04_UnJugadorArrojaLosDadosAvanzaLoQueIndicanLosDados() {
+		Jugador jugador = new JugadorHumano(tablero, new Dinero(100000));
+		Cubilete cubilete = Cubilete.getInstance();
+		cubilete.agregar(new DadoCargado(5));
+		cubilete.lanzar();
+		jugador.avanzar(cubilete.sumarValores());
+		Encasillable carcel = tablero.getFactory().getCarcel();
+		Assert.assertEquals(carcel, jugador.obtenerCasilleroActual());
+	}
+	
+	@Test
+	public void test05_NoSePuedeConstruirHotelEnTerrenoSimple() {
+		
+	}
+	
+	@Test
+	public void test06_ElJugadorNoTienePropiedadesNiDineroQuedaFueraDeLaRonda() {
+		
+	}
+	
+	@Test
+	public void test06_ElJugadorNoTieneDineroVendePropiedadesPeroNoLeAlcanzaQuedaFueraDeLaRonda() {
+		
+	}
+	
+}
