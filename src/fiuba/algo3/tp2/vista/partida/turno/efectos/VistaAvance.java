eefd9ce700ff3586d67014af1e4024169ed1b94d
Matias Ricaldone <mricaldone@gmail.com>
2017-12-05 13:17:18 -0300

    Mejora visual

--- src/fiuba/algo3/tp2/vista/partida/turno/efectos/VistaAvance.java
+++ src/fiuba/algo3/tp2/vista/partida/turno/efectos/VistaAvance.java
@@ -6,9 +6,10 @@ import fiuba.algo3.tp2.vista.partida.turno.LabelTurnoDinamica;
 public class VistaAvance extends VistaAccion {
 	
 	public VistaAvance(int cant_casilleros) {
-		LabelTurnoDinamica label = new LabelTurnoDinamica("Caiste en Avance Dinamico. Avanzaras " + cant_casilleros + " casilleros");
+		LabelTurnoDinamica label1 = new LabelTurnoDinamica("Caiste en Avance Dinamico.");
+		LabelTurnoDinamica label2 = new LabelTurnoDinamica("Avanzaras " + cant_casilleros + " casilleros");
 		BotonAvanceDinamico boton = new BotonAvanceDinamico(cant_casilleros);
-		this.getChildren().addAll(label,boton);
+		this.getChildren().addAll(label1, label2, boton);
 		
 	}
 }
