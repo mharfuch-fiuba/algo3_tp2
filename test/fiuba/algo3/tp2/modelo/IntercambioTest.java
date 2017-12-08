454826e2142b59d71d6db30552907a95fd5aa558
Matias Ricaldone <mricaldone@gmail.com>
2017-12-08 15:13:19 -0300

    Test intercambio

--- test/fiuba/algo3/tp2/modelo/IntercambioTest.java
+++ test/fiuba/algo3/tp2/modelo/IntercambioTest.java
@@ -1,5 +1,9 @@
 package fiuba.algo3.tp2.modelo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import fiuba.algo3.tp2.modelo.encasillables.propiedades.terrenos_dobles.BuenosAiresNorte;
 import fiuba.algo3.tp2.modelo.encasillables.propiedades.terrenos_dobles.BuenosAiresSur;
 import fiuba.algo3.tp2.modelo.tablero.Tablero;
 
@@ -8,37 +12,41 @@ public class IntercambioTest {
 	private Jugador jugador1 = new JugadorHumano(tablero, new Dinero(100000));
 	private Jugador jugador2 = new JugadorHumano(tablero, new Dinero(100000));
 	private BuenosAiresSur bsassur;
+	private BuenosAiresNorte bsasnorte;
 
 	public IntercambioTest() {
 		jugador1.avanzar(2);
 		bsassur = (BuenosAiresSur) jugador1.obtenerCasilleroActual();
 		jugador1.comprarCasilleroActual();
+		jugador2.avanzar(4);
+		bsasnorte = (BuenosAiresNorte) jugador2.obtenerCasilleroActual();
+		jugador2.comprarCasilleroActual();
 	}
-	/*
-	 * @Test public void
-	 * testElJugador1IntercambiaUnaPropiedadPor30000ConElJugador2ElJugador2EsElNuevoPropietario
-	 * () { Intercambio intercambio = jugador1.generarIntercambio(bsassur, new
-	 * Dinero(30000)); intercambio.aceptarIntercambio(jugador2);
-	 * Assert.assertEquals(jugador2, bsassur.getPropietario());
-	 * Assert.assertTrue(jugador2.getPropiedades().contains(bsassur)); }
-	 * 
-	 * @Test public void
-	 * testElJugador1IntercambiaUnaPropiedadPor20000ConElJugador2ElJugador1YaNoEsPropietario
-	 * () { Intercambio intercambio = jugador1.generarIntercambio(bsassur, new
-	 * Dinero(20000)); intercambio.aceptarIntercambio(jugador2);
-	 * Assert.assertNotEquals(jugador1, bsassur.getPropietario());
-	 * Assert.assertFalse(jugador1.getPropiedades().contains(bsassur)); }
-	 * 
-	 * @Test public void
-	 * testElJugador1IntercambiaUnaPropiedadPor30000ConElJugador2ElJugador2Pierde30000
-	 * () { Intercambio intercambio = jugador1.generarIntercambio(bsassur, new
-	 * Dinero(30000)); intercambio.aceptarIntercambio(jugador2);
-	 * Assert.assertEquals(jugador2, bsassur.getPropietario()); }
-	 * 
-	 * @Test public void
-	 * testElJugador1IntercambiaUnaPropiedadPor20000ConElJugador2ElJugador1Gana20000
-	 * () { Intercambio intercambio = jugador1.generarIntercambio(bsassur, new
-	 * Dinero(20000)); intercambio.aceptarIntercambio(jugador2);
-	 * Assert.assertEquals(jugador2, bsassur.getPropietario()); }
-	 */
+	
+	@Test
+	public void testJugador1IntercambiaBsAsSurPorBsAsNorteJugador2EsPropietarioDeBsAsSur() {
+		Intercambio intercambio = new Intercambio(bsasnorte, bsassur);
+		intercambio.aceptarIntercambio();
+		Assert.assertEquals(jugador2, bsassur.getPropietario());
+	}
+	
+	@Test
+	public void testJugador1IntercambiaBsAsSurPorBsAsNorteJugador1EsPropietarioDeBsAsNorte() {
+		Intercambio intercambio = new Intercambio(bsasnorte, bsassur);
+		intercambio.aceptarIntercambio();
+		Assert.assertEquals(jugador1, bsasnorte.getPropietario());
+	}
+	
+	@Test
+	public void testJugador1NoIntercambiaBsAsSurPorBsAsNorteJugador1EsPropietarioDeBsAsSur() {
+		new Intercambio(bsasnorte, bsassur);
+		Assert.assertEquals(jugador1, bsassur.getPropietario());
+	}
+	
+	@Test
+	public void testJugador1NoIntercambiaBsAsSurPorBsAsNorteJugador2EsPropietarioDeBsAsNorte() {
+		new Intercambio(bsasnorte, bsassur);
+		Assert.assertEquals(jugador2, bsasnorte.getPropietario());
+	}
+	
 }
